8c12478648175dd374f7e3e5e6287b4ec7aeda61
onpi <o.ryota1001@gmail.com>
2020-08-14 16:34:27 +0900

    ドット連携完成

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,7 @@ let activeIndex = 0;
 let dotsHTML = '';
 // 画像の数だけドット表示
 window.onload = function() {
+  sliderArray[activeIndex].classList.add('active');
   dotsHTML += '<div class="dots_wrap">';
   for (let i = 0; i < sliderArray.length; i++) {
     dotsHTML += '<button type="button" class="dots" data-number="'+i+'"></button>';
@@ -18,46 +19,40 @@ window.onload = function() {
   dots[activeIndex].classList.add('active');// 1つ目のdotsに.active付与
 };
 
-
-
-sliderArray[activeIndex].classList.add('active');
-
 function nextImage(e){
   sliderArray[activeIndex].classList.remove('active');
   dots[activeIndex].classList.remove('active');
-
+  let dataNumber = e.target.dataset.number;// ドットのdata属性取得
   let sliderCount = sliderArray.length;
-  if(e.target.id == 'prev') {
-    activeIndex -= 1;
+  activeIndex = activeIndex;
+  if(e.target.className === 'dots'){
+    // スライダー切り替え
+    sliderArray[activeIndex].classList.remove('active');
+    sliderArray[dataNumber].classList.add('active');
+    // ドット切り替え
+    dots[activeIndex].classList.remove('active');
+    dots[dataNumber].classList.add('active');
+    // console.log(dataNumber);
+    activeIndex = dataNumber;
+    // return activeIndex;
+  }else if(e.target.id === 'prev') {
+    activeIndex --;
     if(activeIndex < 0) {
       activeIndex = sliderCount - 1;
     }
-    
-  }else if(e.target.id == 'next'){
-    activeIndex += 1;
+  }else if(e.target.id === 'next'){
+    activeIndex ++;
     if(activeIndex > sliderCount - 1) {
       activeIndex = 0;
     }
-  }else if(e.target.className == 'dots'){
-    let dataNumber = e.target.dataset.number;// ドットのdata属性取得
-    sliderArray[activeIndex].classList.remove('active');
-    sliderArray[dataNumber].classList.add('active');
-    dots[activeIndex].classList.remove('active');
-    dots[dataNumber].classList.add('active');
-    console.log(dataNumber);
-    activeIndex = dataNumber;
-    return activeIndex;
-  }else{
-
   }
+
   dots[activeIndex].classList.add('active');
   sliderArray[activeIndex].classList.add('active');
-  console.log(activeIndex);
 }
 
 document.querySelector('#prev').addEventListener('click', nextImage);
 document.querySelector('#next').addEventListener('click', nextImage);
 document.querySelector('#dots').addEventListener('click', nextImage);
-// const clickDots = document.getElementById('dots');
 
 
